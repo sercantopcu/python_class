081160faa1290a216dd5d020b127fe66e6366d28
somename <a@gmail.com>
2022-10-24 17:20:57 +0000

    added functions class5

---
+++ class5/converter.py
@@ -0,0 +1,19 @@
+def convert_to_list(PARAMETER1):
+    PARAMETER1 = list(PARAMETER1)
+    print(PARAMETER1)
+
+
+
+def convert_to_tuple(PARAMETER1):
+    PARAMETER1 = tuple(PARAMETER1)
+    print(PARAMETER1)
+
+
+def convert_to_str(PARAMETER1):
+    PARAMETER1 = str(PARAMETER1)
+    print(PARAMETER1)
+
+
+def convert_to_dict(PARAMETER1):
+    PARAMETER1 = dict(PARAMETER1)
+    print(PARAMETER1)
---
+++ class5/function_caller.py
@@ -0,0 +1,22 @@
+# import statement
+from first_function import greetings
+from math_functions import *
+import full_function
+from converter import *
+from age_calculator import game
+
+
+
+
+greetings("Sercan")
+multiplication(4, 5)
+addition(1, 2)
+substraction(4, 2)
+division(10, 2)
+FRUIT = ("apple", "pear")
+convert_to_list(FRUIT)
+
+convert_to_str(10)
+game()
+
+full_function.function1()
